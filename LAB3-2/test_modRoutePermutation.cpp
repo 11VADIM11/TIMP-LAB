8d3fa8e06245bacecca669da285feebff2bcb0a6
11VADIM11 <vk5736072@gmail.com>
2024-11-23 13:16:30 +0300

    Update test_modRoutePermutation.cpp

--- LAB3-2/test_modRoutePermutation.cpp
+++ LAB3-2/test_modRoutePermutation.cpp
@@ -6,21 +6,21 @@
 TEST(TestEncryptEmptyText)
 {
     modRoutePermutationCipher cipher(L"456");               
-    CHECK_THROW(cipher.encrypt(L""), std::invalid_argument); // исключение
+    CHECK_THROW(cipher.encrypt(L""), std::invalid_argument); 
 }
 
 // Тест на дешифрование пустого текста с проверкой исключения
 TEST(TestDecryptEmptyText)
 {
     modRoutePermutationCipher cipher(L"456");               
-    CHECK_THROW(cipher.decrypt(L""), std::invalid_argument); // исключение
+    CHECK_THROW(cipher.decrypt(L""), std::invalid_argument); 
 }
 
 // Тест на шифрование текста с невалидным символом
 TEST(TestForeignCharacterInText)
 {
     modRoutePermutationCipher cipher(L"456");                     
-    CHECK_THROW(cipher.encrypt(L"World"), std::invalid_argument); // исключение
+    CHECK_THROW(cipher.encrypt(L"World"), std::invalid_argument); 
 }
 
 // Тест на правильность шифрования
@@ -42,8 +42,8 @@ TEST(TestDecryptionCorrectness)
 // Тест на обработку некорректного ключа (неположительное число)
 TEST(TestInvalidKey)
 {
-    CHECK_THROW(modRoutePermutationCipher cipher(L"-1"), std::invalid_argument);   // исключение
-    CHECK_THROW(modRoutePermutationCipher cipher(L"xyz"), std::invalid_argument); // исключение
+    CHECK_THROW(modRoutePermutationCipher cipher(L"-1"), std::invalid_argument);   
+    CHECK_THROW(modRoutePermutationCipher cipher(L"xyz"), std::invalid_argument); 
 }
 
 // Тест на шифрование с валидным текстом
@@ -51,16 +51,16 @@ TEST(TestEncryptionWithValidText)
 {
     modRoutePermutationCipher cipher(L"456"); 
     std::wstring encrypted = cipher.encrypt(L"ЗУЖФУ");
-    CHECK(encrypted != L"ДОБРО"); // Убеждаемся, что текст действительно зашифрован
+    CHECK(encrypted != L"ДОБРО"); 
 }
 
 // Тест на дешифрование с валидным текстом
 TEST(TestDecryptionWithValidText)
 {
     modRoutePermutationCipher cipher(L"456");
-    std::wstring encrypted = cipher.encrypt(L"ДОБРО");
+    std::wstring encrypted = cipher.encrypt(L"ЗУЖФУ");
     std::wstring decrypted = cipher.decrypt(encrypted);
-    CHECK(decrypted == L"ЗУЖФУ"); // Убеждаемся, что мы можем расшифровать обратно
+    CHECK(decrypted == L"ДОБРО"); 
 }
 
 int main()
